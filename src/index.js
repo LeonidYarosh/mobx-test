280e69714cbdf8f1ac43b67ecbf395b1fdc0feae
Leonid_Yarosh <leo2894@mail.ru>
2020-03-29 15:42:22 +0300

    simple store on mobx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+
+import {observable, computed, extendObservable} from 'mobx';
+import { observer } from 'mobx-react';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const nickName = observable({
+  firstName: 'Leonid',
+  age: 26,
+
+  get nickName() {
+    console.log('Generate nickName!');
+    return `${this.firstName}${this.age}`;
+  },
+  increment() { this.age++ },
+  decrement() { this.age-- },
+
+})
+
+const todos = observable([
+  { text: 'Learn React' },
+  { text: 'Learn Mobx' },
+])
+
+@observer class Counter extends Component {
+
+  handleIncrement = () => { this.props.store.increment() };
+  handleDecrement = () => { this.props.store.decrement() };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.store.nickName}</h1>
+        <h1>{this.props.store.age}</h1>
+        <button onClick={this.handleDecrement}>-1</button>
+        <button onClick={this.handleIncrement}>+1</button>
+
+        <ul>
+          {todos.map(({text}) =>
+            <li key={text} >{text}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Counter store={nickName} />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+todos.push({text: 'Learn Redux'})
