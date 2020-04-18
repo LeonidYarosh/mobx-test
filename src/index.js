ccdcc6b56ddc636b84ab4d181852b65919c46a24
Leonid_Yarosh <leo2894@mail.ru>
2020-04-18 19:52:52 +0300

    Set customer actions

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
 
-import {observable, computed, extendObservable} from 'mobx';
-import { observer } from 'mobx-react';
+import { observable, action, when, autorun } from 'mobx'
+import { observer } from 'mobx-react'
 
-import './index.css';
+import './index.css'
 
-const nickName = observable({
-  firstName: 'Leonid',
-  age: 26,
-
-  get nickName() {
-    console.log('Generate nickName!');
-    return `${this.firstName}${this.age}`;
-  },
-  increment() { this.age++ },
-  decrement() { this.age-- },
-
-})
+class Store {
+  @observable count = 0
+  @action increment() { this.count++ }
+  @action decrement() { this.count-- }
+}
 
 const todos = observable([
   { text: 'Learn React' },
   { text: 'Learn Mobx' },
 ])
 
+const appStore = new Store()
+
+when(
+  () => appStore.count > 5,
+  () => { alert('Сработало')}
+)
+
+autorun(() => {
+  alert(`Count value is: ${appStore.count}`)
+}, {
+  name: 'Custom autorun',
+  delay: 3000,
+})
+
 @observer class Counter extends Component {
+  static propTypes = {
+    store: PropTypes.object,
+  }
 
   handleIncrement = () => { this.props.store.increment() };
   handleDecrement = () => { this.props.store.decrement() };
@@ -32,24 +43,23 @@ const todos = observable([
   render() {
     return (
       <div className="App">
-        <h1>{this.props.store.nickName}</h1>
-        <h1>{this.props.store.age}</h1>
+        <h1>{this.props.store.count}</h1>
         <button onClick={this.handleDecrement}>-1</button>
         <button onClick={this.handleIncrement}>+1</button>
 
         <ul>
-          {todos.map(({text}) =>
+          {todos.map(({ text }) =>
             <li key={text} >{text}</li>
           )}
         </ul>
       </div>
-    );
+    )
   }
 }
 
 ReactDOM.render(
-  <Counter store={nickName} />,
+  <Counter store={appStore} />,
   document.getElementById('root')
-);
+)
 
-todos.push({text: 'Learn Redux'})
+todos.push({ text: 'Learn Redux' })
